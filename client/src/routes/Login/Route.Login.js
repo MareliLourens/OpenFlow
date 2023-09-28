e460b72c635652c1c3d349a5d553fe213b277cad
Godwin Cameron <71267628+GodwinCameron@users.noreply.github.com>
2023-09-28 14:52:42 +0200

    User setup

--- client/src/routes/Login/Route.Login.js
+++ client/src/routes/Login/Route.Login.js
@@ -1,25 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Style.Login.module.scss";
 
 const LoginRoute = () => {
+
+  const [signOption, setSignOption] = useState('signIn');
+
+  const changeSignUp = (option) => {
+    setSignOption(option);
+    console.log(option);
+  }
+
+
+
+
   return (
     <div className={style.main}>
       <div className={style.loginBlock}>
         <div className={style.topSection}>
-          <div className={style.signOpt}>
+          <div onClick={() => changeSignUp('signUp')} className={style.signOpt}>
             Sign Up
           </div>
-          <div className={[style.signOpt, style.signOpt2].join(' ')}>
+          <div onClick={() => changeSignUp('signIn')} className={[style.signOpt, style.signOpt2].join(' ')}>
             Sign In
           </div>
         </div>
-        <div className={style.form}>
+        {signOption === 'signIn' ? (<form className={style.form1}>
           <p>Your Email</p>
           <input placeholder="Enter your Email" className={style.input}></input>
-          <p>Your Password</p>
+          <p className={style.align}>Your Password</p>
           <input placeholder="Enter your Password" className={style.input}></input>
           <button className={style.button}>Submit</button>
-        </div>
+        </form>)
+        :
+        (<form className={[style.form1, style.form2].join(' ')}>
+          <p>First Name</p>
+          <input placeholder="Enter your Email" className={style.input}></input>
+          <p>Surname</p>
+          <input placeholder="Enter your Email" className={style.input}></input>
+          <p>Your Email</p>
+          <input placeholder="Enter your Email" className={style.input}></input>
+          <p className={style.align}>Your Password</p>
+          <input placeholder="Enter your Password" className={style.input}></input>
+          <button className={style.button}>Sign Up!</button>
+        </form>)}
       </div>
     </div>
   );
