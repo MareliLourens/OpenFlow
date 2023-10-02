4faa569d2b5d2c13e9488b4ff6cf67c73474cbe7
Godwin Cameron <71267628+GodwinCameron@users.noreply.github.com>
2023-10-02 13:42:32 +0200

    Added Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomeRoute from './routes/Home/Route.Home';
 import LoginRoute from './routes/Login/Route.Login';
+import ProfileRoute from './routes/Profile/Route.Profile';
+import QuestionsRoute from './routes/Questions/Route.Questions';
+import AnswersRoute from './routes/Answers/Route.Answers';
+import SupportRoute from './routes/Support/Route.Support';
 
 function App() {
   return (
@@ -8,6 +12,10 @@ function App() {
       <Routes>
         <Route path="/" element={<HomeRoute/>}/>
         <Route path="/login" element={<LoginRoute/>}/>
+        <Route path="/profile" element={<ProfileRoute/>}/>
+        <Route path="/questions" element={<QuestionsRoute/>}/>
+        <Route path="/answers" element={<AnswersRoute/>}/>
+        <Route path="/support" element={<SupportRoute/>}/>  
       </Routes>
     </BrowserRouter>
   );
