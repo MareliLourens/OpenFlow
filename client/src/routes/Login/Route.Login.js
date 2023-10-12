92550bc2b551dc17aaffc4bb112e1a47c9a341b0
Tebogo Ramolobeng <221244@virtualwindow.co.za>
2023-10-13 00:39:20 +0200

    commit

--- client/src/routes/Login/Route.Login.js
+++ client/src/routes/Login/Route.Login.js
@@ -1,48 +1,73 @@
 import React, { useState } from "react";
 import style from "./Style.Login.module.scss";
+import loginLogo from "../../assets/images/Login-Logo.svg";
 
 const LoginRoute = () => {
-
-  const [signOption, setSignOption] = useState('signIn');
-
-  const changeSignUp = (option) => {
-    setSignOption(option);
-    console.log(option);
-  }
-
-
-
+  const [signOption, setSignOption] = useState("signIn");
 
   return (
     <div className={style.main}>
       <div className={style.loginBlock}>
-        <div className={style.topSection}>
-          <div onClick={() => changeSignUp('signUp')} className={style.signOpt}>
-            Sign Up
-          </div>
-          <div onClick={() => changeSignUp('signIn')} className={[style.signOpt, style.signOpt2].join(' ')}>
-            Sign In
-          </div>
+        <div className={style.formContainer}>
+          {signOption === "signIn" ? (
+            <form className={style.form1}>
+              <h2>Welcome</h2>
+              <h3>Login to continue</h3>
+              <p>Your Email</p>
+              <input
+                placeholder="Enter your Email"
+                className={style.input}
+              ></input>
+              <p className={style.align}>Your Password</p>
+              <input
+                placeholder="Enter your Password"
+                className={style.input}
+              ></input>
+ 
+
+            <div class="form-check d-flex justify-content-start mb-4">
+              <input class="form-check-input" type="checkbox" value="" id="form1Example3" />
+              <label class="form-check-label" for="form1Example3"> Remember password </label>
+            </div>
+              
+              <button className={style.button}>Submit</button>
+            </form>
+          ) : (
+            <form className={[style.form1, style.form2].join(" ")}>
+              <h2>Welcome</h2>
+              <h3>Create an Account</h3>
+              <p>First Name</p>
+              <input
+                placeholder="Enter your First Name"
+                className={style.input}
+              ></input>
+              <p>Surname</p>
+              <input
+                placeholder="Enter your Surname"
+                className={style.input}
+              ></input>
+              <p>Your Email</p>
+              <input
+                placeholder="Enter your Email"
+                className={style.input}
+              ></input>
+              <p className={style.align}>Your Password</p>
+              <input
+                placeholder="Enter your Password"
+                className={style.input}
+              ></input>
+              <button className={style.button}>Sign Up!</button>
+            </form>
+          )}
         </div>
-        {signOption === 'signIn' ? (<form className={style.form1}>
-          <p>Your Email</p>
-          <input placeholder="Enter your Email" className={style.input}></input>
-          <p className={style.align}>Your Password</p>
-          <input placeholder="Enter your Password" className={style.input}></input>
-          <button className={style.button}>Submit</button>
-        </form>)
-        :
-        (<form className={[style.form1, style.form2].join(' ')}>
-          <p>First Name</p>
-          <input placeholder="Enter your Email" className={style.input}></input>
-          <p>Surname</p>
-          <input placeholder="Enter your Email" className={style.input}></input>
-          <p>Your Email</p>
-          <input placeholder="Enter your Email" className={style.input}></input>
-          <p className={style.align}>Your Password</p>
-          <input placeholder="Enter your Password" className={style.input}></input>
-          <button className={style.button}>Sign Up!</button>
-        </form>)}
+      </div>
+      <div className={style.imageContainer}>
+        {/* Add your image component here */}
+        <img
+          src={loginLogo}
+          alt="Login Image"
+          className={style.image}
+        />
       </div>
     </div>
   );
--- client/src/routes/Profile/Route.Profile.js
+++ client/src/routes/Profile/Route.Profile.js
@@ -1,13 +1,50 @@
 import React from "react";
-import style from "./Style.Profile.module.scss";
-import NavBarComponent from "../../components/NavBar-Component/Component.NavBar";
+import style from "./Style.Profile.module.scss"; // Import the correct style
+import loginLogo from "../../assets/images/Login-Logo.svg";
 
 const ProfileRoute = () => {
   return (
     <div className={style.main}>
-      <div className={style.content}>
-        <NavBarComponent />
-        <h1 className={style.heading}> Profile </h1>
+      <div className={style.loginBlock}>
+        {/* Remove the "Sign Up" button from the top section */}
+        <div className={style.topSection}>
+          {/* No need for the "Sign Up" button here */}
+        </div>
+        <div className={style.formContainer}>
+          <form className={[style.form1, style.form2].join(" ")}>
+          <p>Back</p>
+            <h2>Create an account</h2>
+            <h3>Fill in the details to get started</h3>
+            <p>First Name</p>
+            <input
+              placeholder="Enter your First Name"
+              className={style.input}
+            ></input>
+            <p>Surname</p>
+            <input
+              placeholder="Enter your Surname"
+              className={style.input}
+            ></input>
+            <p>Your Email</p>
+            <input
+              placeholder="Enter your Email"
+              className={style.input}
+            ></input>
+            <p className={style.align}>Your Password</p>
+            <input
+              placeholder="Enter your Password"
+              className={style.input}
+            ></input>
+            <button className={style.button}>Sign Up!</button>
+          </form>
+        </div>
+      </div>
+      <div className={style.imageContainer}>
+        <img
+          src={loginLogo}
+          alt="Login Image"
+          className={style.image}
+        />
       </div>
     </div>
   );
