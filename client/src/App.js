28837c43c54aaf4d92164835217321a9b75e68d5
MareliLourens <221119@virtualwindow.co.za>
2023-11-10 16:39:21 +0200

    Admin Page

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,10 @@ import ProfileRoute from './routes/Profile/Route.Profile';
 import QuestionsRoute from './routes/Questions/Route.Questions';
 import AnswersRoute from './routes/Answers/Route.Answers';
 import SupportRoute from './routes/Support/Route.Support';
-<<<<<<< Updated upstream
-=======
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import SingleQuestionRoute from './routes/SingleQuestion/Route.SingleQuestion';
 import Admin from './routes/Admin/Route.Admin';
->>>>>>> Stashed changes
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import SingleQuestionRoute from './routes/SingleQuestion/Route.SingleQuestion';
 
 function App() {
   const [admin, setAdmin] = useState(false);
@@ -45,14 +39,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-<<<<<<< Updated upstream
-        <Route path="/" element={<HomeRoute/>}/>
-        <Route path="/login" element={<LoginRoute/>}/>
-        <Route path="/profile" element={<ProfileRoute/>}/>
-        <Route path="/questions" element={<QuestionsRoute/>}/>
-        <Route path="/answers" element={<AnswersRoute/>}/>
-        <Route path="/support" element={<SupportRoute/>}/>  
-=======
+
         <Route path="/" element={<HomeRoute admin={admin} user={user}/>}/>
         <Route path="/login" element={<LoginRoute admin={admin} user={user}/>}/>
         <Route path="/profile" element={<ProfileRoute admin={admin} user={user}/>}/>
@@ -61,14 +48,6 @@ function App() {
         <Route path="/support" element={<SupportRoute admin={admin} user={user}/>}/>
         <Route path="/singlequestion" element={<SingleQuestionRoute admin={admin} user={user}/>}/>
         <Route path="/admin" element={<Admin admin={admin}/>}/>
->>>>>>> Stashed changes
-        <Route path="/" element={<HomeRoute admin={admin} user={user}/>}/>
-        <Route path="/login" element={<LoginRoute admin={admin} user={user}/>}/>
-        <Route path="/profile" element={<ProfileRoute admin={admin} user={user}/>}/>
-        <Route path="/questions" element={<QuestionsRoute admin={admin} user={user}/>}/>
-        <Route path="/answers" element={<AnswersRoute admin={admin} user={user}/>}/>
-        <Route path="/support" element={<SupportRoute admin={admin} user={user}/>}/>
-        <Route path="/singlequestion" element={<SingleQuestionRoute admin={admin} user={user}/>}/>
       </Routes>
     </BrowserRouter>
   );
