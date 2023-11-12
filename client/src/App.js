e355dbc147863571bfa2bca3e693f59d735e8221
MareliLourens <221119@virtualwindow.co.za>
2023-11-12 16:03:32 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,26 +5,13 @@ import ProfileRoute from './routes/Profile/Route.Profile';
 import QuestionsRoute from './routes/Questions/Route.Questions';
 import AnswersRoute from './routes/Answers/Route.Answers';
 import SupportRoute from './routes/Support/Route.Support';
-<<<<<<< Updated upstream
-=======
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import SingleQuestionRoute from './routes/SingleQuestion/Route.SingleQuestion';
 import Admin from './routes/Admin/Route.Admin';
->>>>>>> Stashed changes
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-<<<<<<< Updated upstream
-        <Route path="/" element={<HomeRoute/>}/>
-        <Route path="/login" element={<LoginRoute/>}/>
-        <Route path="/profile" element={<ProfileRoute/>}/>
-        <Route path="/questions" element={<QuestionsRoute/>}/>
-        <Route path="/answers" element={<AnswersRoute/>}/>
-        <Route path="/support" element={<SupportRoute/>}/>  
-=======
         <Route path="/" element={<HomeRoute admin={admin} user={user}/>}/>
         <Route path="/login" element={<LoginRoute admin={admin} user={user}/>}/>
         <Route path="/profile" element={<ProfileRoute admin={admin} user={user}/>}/>
@@ -33,7 +20,6 @@ function App() {
         <Route path="/support" element={<SupportRoute admin={admin} user={user}/>}/>
         <Route path="/singlequestion" element={<SingleQuestionRoute admin={admin} user={user}/>}/>
         <Route path="/admin" element={<Admin admin={admin}/>}/>
->>>>>>> Stashed changes
       </Routes>
     </BrowserRouter>
   );
