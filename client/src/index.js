1d0842ac5cb956ea278a48b79d8b992c97701107
Tebogo Ramolobeng <221244@virtualwindow.co.za>
2023-11-14 00:56:10 +0200

    Commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
-// src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'; // adjust the import based on your actual file structure
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // </React.StrictMode>
 );
